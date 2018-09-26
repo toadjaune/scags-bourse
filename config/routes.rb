306676d59f2dd42dcdab84996379ed2eab1c01b1
Arnaud Venturi <arnaud.venturi@rez-gif.supelec.fr>
2018-09-26 17:20:14 +0200

    Start Integrating User properly

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # Normal devise routes for a user login/logout, etc…
+  devise_for :users
   root 'base#index'
 end
