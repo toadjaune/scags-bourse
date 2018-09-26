b9c362bc1f8f9cb97058d7178e8dd154eb9ab78b
Arnaud Venturi <arnaud.venturi@rez-gif.supelec.fr>
2018-09-26 18:03:45 +0200

    A few basic operations on a user

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 
   # Normal devise routes for a user login/logout, etc…
   devise_for :users
+  # Classic routes for a user
+  get 'users', to: 'users#index'
+
   root 'base#index'
 end
