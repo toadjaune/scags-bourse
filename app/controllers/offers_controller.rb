e6a6d8c82abf9b44ad63308fd6dd103a1db94322
Arnaud Venturi <arnaud.venturi@rez-gif.supelec.fr>
2018-09-27 20:11:46 +0200

    Start enforcing user login

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,4 +1,5 @@
 class OffersController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_offer, only: [:show, :edit, :update, :destroy, :purge_images]
 
   # GET /offers
