69d065cabf3f5d85b8c47b487ebe7b2d071b8e28
Arnaud Venturi <arnaud.venturi@rez-gif.supelec.fr>
2018-09-27 20:38:56 +0200

    Add some fields on user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,17 +2,29 @@
 #
 # Table name: users
 #
-#  id                  :bigint(8)        not null, primary key
-#  email               :string           default(""), not null
-#  encrypted_password  :string           default(""), not null
-#  remember_created_at :datetime
-#  sign_in_count       :integer          default(0), not null
-#  current_sign_in_at  :datetime
-#  last_sign_in_at     :datetime
-#  current_sign_in_ip  :inet
-#  last_sign_in_ip     :inet
-#  created_at          :datetime         not null
-#  updated_at          :datetime         not null
+#  id                     :bigint(8)        not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :inet
+#  last_sign_in_ip        :inet
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  confirmation_token     :string
+#  confirmed_at           :datetime
+#  confirmation_sent_at   :datetime
+#  unconfirmed_email      :string
+#  failed_attempts        :integer          default(0), not null
+#  unlock_token           :string
+#  locked_at              :datetime
+#  name                   :string
+#  phone                  :string
+#  admin                  :boolean          default(FALSE)
 #
 
 require 'rails_helper'
