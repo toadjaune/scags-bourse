0374c6c2bf6ada2aa77c84ec3c0390bfd2bf886e
Arnaud Venturi <arnaud.venturi@rez-gif.supelec.fr>
2018-09-27 22:42:13 +0200

    Some prod configuration changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,4 +32,11 @@ class User < ApplicationRecord
   # :lockable, :timeoutable, :registerable, and :omniauthable
   devise :database_authenticatable, :confirmable, :rememberable, :trackable, :validatable, :recoverable
 
+  def to_s
+    if name
+      return name
+    else
+      return email
+    end
+  end
 end
